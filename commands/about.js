a3cf7a2690caa315223c10da2a7d36f7b7c7522c
gavin <adstarr6@gmaill.com>
2021-01-28 19:28:00 -0800

    idk even know

--- commands/about.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-exports.run = async (settings, client, message, args) => {    
-    const serverEmbedNoIcon = new Discord.RichEmbed().setTitle(
-        'title here').setColor(embedColor)
-        .setDescription('put things here')
-    message.channel.send(serverEmbedNoIcon);
-
-}
